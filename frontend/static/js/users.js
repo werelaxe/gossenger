9e42295fabd06324f95add1bb398b5eda1a4158d
Daniil Sharko <daniil.sharko@jetbrains.com>
2020-05-15 21:32:58 +0300

    Add users search by api filter

--- frontend/static/js/users.js
+++ frontend/static/js/users.js
@@ -1,18 +1,21 @@
 function addUser(id, nickname, first_name, last_name) {
-    const usersDiv = $("#users");
-    const newRowElement = $(`<tr><td>${id}</td><td><a href="/user_page?user_id=${id}">${nickname}</a></td><td>${first_name}</td><td>${last_name}</td></tr>`);
-    usersDiv.append(newRowElement);
+    const usersContent = $("#users");
+    const newRowElement = $(`<tr><td><a href="/user_page?user_id=${id}">${nickname}</a></td><td>${first_name}</td><td>${last_name}</td></tr>`);
+    usersContent.append(newRowElement);
 }
 
 
-function setListUsersHandler() {
-    $.get("/users/list")
+function setListUsersHandler(filter) {
+    console.log("call it: " + filter);
+    $.get("/users/search?filter=" + filter)
         .fail(function (data) {
             console.log("Fail while loading users");
             console.log(data)
         })
         .done(function (data) {
             const users = JSON.parse(data);
+            const usersContent = $("#users");
+            usersContent.empty();
             users.forEach(function (user) {
                 addUser(user["id"], user["nickname"], user["first_name"], user["last_name"]);
             })
@@ -20,6 +23,17 @@ function setListUsersHandler() {
 }
 
 
+function setSearchHandler() {
+    const usersFilterFld = $("#users-filter-fld");
+    usersFilterFld.on("keyup paste", function () {
+        const searchFilter = usersFilterFld.val();
+        if (searchFilter.length >= 3) {
+            setListUsersHandler(searchFilter);
+        }
+    });
+}
+
+
 $(document).ready(function() {
-    setListUsersHandler();
+    setSearchHandler();
 });
